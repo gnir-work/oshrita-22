ec4971da9ae6d730941ed518b80792f55f5375cb
Nir Geller <gnir.work@gmail.com>
2020-07-15 17:29:36 +0300

    Added crack animation.

--- src/components/Egg.jsx
+++ src/components/Egg.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import _, { random } from 'lodash';
+import _, { random } from "lodash";
 
 import "./Egg.scss";
 
@@ -7,28 +7,31 @@ const NUMBER_OF_CRACKS = 5;
 
 export const Egg = () => {
     const [cracks, setCracks] = useState([]);
+    const [isShaking, setIsShaking] = useState(false);
 
     const handleHit = () => {
         if (cracks.length === NUMBER_OF_CRACKS) {
             return;
         }
-        
         let crack = random(1, NUMBER_OF_CRACKS);
         while (cracks.includes(crack)) {
             crack = random(1, NUMBER_OF_CRACKS);
         }
+        setIsShaking(true);
+        setTimeout(() => {
+            setIsShaking(false);
+        }, 500);
         setCracks([...cracks, crack]);
-    }
+    };
 
     return (
-        <div className="egg-container">
-            <div onClick={handleHit} className="egg">
-                {
-                    cracks.map(crackNumber => (
-                        <div className={`crack crack${crackNumber}`} />
-                    ))
-                }
-            </div>
+        <div
+            onClick={handleHit}
+            className={`egg ${isShaking ? "shake-animation" : ""}`}
+        >
+            {cracks.map((crackNumber) => (
+                <div className={`crack crack${crackNumber}`} />
+            ))}
         </div>
     );
 };
