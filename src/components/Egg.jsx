73fa82678d31d347d35fdd84b35edd8a8893819a
Nir Geller <gnir.work@gmail.com>
2020-07-15 16:20:19 +0300

    Added several cracks on egg.

--- src/components/Egg.jsx
+++ src/components/Egg.jsx
@@ -4,6 +4,13 @@ import "./Egg.scss";
 
 export const Egg = () => (
     <div className="egg-container">
-        <div className="egg crack1" />
+        <div className="egg">
+            <div className="crack crack1" />
+            <div className="crack crack2" />
+            <div className="crack crack3" />
+            <div className="crack crack4" />
+            <div className="crack crack5" />
+            <div className="crack crack6" />
+        </div>
     </div>
 );
