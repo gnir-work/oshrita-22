761fa1506c7e4e1b971ef4ae7e2f6d57131f9e57
Nir Geller <gnir.work@gmail.com>
2020-08-28 10:43:47 +0300

    Updates

---
+++ precache-manifest.38f3579cd4709b547494b88500983439.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4447f7a3cac304a2c16d811d32b171ce",
+    "url": "/oshrita-22/index.html"
+  },
+  {
+    "revision": "e261bbe66cd006088c35",
+    "url": "/oshrita-22/static/css/main.02e5dd73.chunk.css"
+  },
+  {
+    "revision": "d8c5f75b10ba2d9f5b31",
+    "url": "/oshrita-22/static/js/2.4c9b42ea.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/oshrita-22/static/js/2.4c9b42ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e261bbe66cd006088c35",
+    "url": "/oshrita-22/static/js/main.4937cd33.chunk.js"
+  },
+  {
+    "revision": "bd3c144061c7fe2f8338",
+    "url": "/oshrita-22/static/js/runtime-main.4db67527.js"
+  },
+  {
+    "revision": "e976e4add5d8eafb08dfada05f57504b",
+    "url": "/oshrita-22/static/media/target.e976e4ad.svg"
+  }
+]);
